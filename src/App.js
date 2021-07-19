11e4f26533ded2f66f699b146386f770f9c82e0b
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2021-07-19 15:07:58 +0400

    new Component : <Header/>, new Container : <Home/>

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import "./App.scss";
 import axios from "axios";
 
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
 
+import "./Components/Header/Header.scss";
 import Header from "./Components/Header/Header";
-//import Home from "./containers/Home";
+import Home from "./containers/Home";
 //import Offer from "./containers/Offer";
 
 import "./containers/Home.scss";
 
 import formatPrice from "./helpers/formatPrice";
 
-function App() {
+const App = () => {
   const [offers, setOffers] = useState([]);
   const [numberOfOffers, setNumberOfOffers] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
@@ -24,9 +31,8 @@ function App() {
         "https://lereacteur-vinted-api.herokuapp.com/offers"
       );
 
-      console.log(response);
-
       setOffers(response.data.offers);
+
       setNumberOfOffers(offers.length);
 
       setIsLoading(false);
@@ -37,18 +43,8 @@ function App() {
   return (
     <div className="app">
       <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/offer">Offer</Link>
-            </li>
-          </ul>
-        </nav>
         <Switch>
-          <Route path="/offer">
+          <Route path="/offer/:id">
             <Offer isLoading={isLoading} offer={offers[0]} />
           </Route>
           <Route path="/">
@@ -58,52 +54,47 @@ function App() {
       </Router>
     </div>
   );
-}
-
-const Home = (props) => {
-  const { isLoading, offers } = props;
-
-  return (
-    <div className="home">
-      <Header />
-      <div className="ready">
-        <div>
-          <p>Prêts à faire du tri dans vos placards ?</p>
-          <button>Commencer à vendre</button>
-        </div>
-      </div>
-      <div className="offers">
-        {isLoading
-          ? "Chargement en cours"
-          : offers.map((e, i) => (
-              <div key={i} className="offer-item">
-                <div key={e._id}>{e.owner.account.username}</div>
-                <img src={e.product_image.secure_url} alt="" />
-                <div>
-                  {new Intl.NumberFormat("fr-FR", {
-                    style: "currency",
-                    currency: "EUR",
-                  }).format(e.product_price)}
-                </div>
-                <div>{e.product_details[0].MARQUE}</div>
-                <div>{e.product_details[1].TAILLE}</div>
-              </div>
-            ))}
-      </div>
-    </div>
-  );
 };
 
-const Offer = ({ offer, isLoading }) => {
+const Offer = () => {
+  const { id } = useParams();
+
+  const [offer, setOffer] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData(id) {
+      setIsLoading(true);
+
+      const response = await axios.get(
+        "https://lereacteur-vinted-api.herokuapp.com/offer/" + id
+      );
+
+      setOffer(response.data);
+
+      setIsLoading(false);
+    }
+    fetchData(id);
+  }, [id]);
+
   return (
     <div className="offer">
+      <Header />
       {isLoading ? (
         "Chargement en cours"
       ) : (
         <div>
           <img src={offer.product_image.secure_url} alt="offer" />
-          <div>{offer.product_name}</div>
+
           <div>{formatPrice(offer.product_price)}</div>
+          <ul>
+            {offer.product_details.map((e, i) => (
+              <li>{`${Object.keys(e)} : ${Object.values(e)}`}</li>
+            ))}
+          </ul>
+          <div>{offer.product_name}</div>
+          <div>{offer.product_description}</div>
+          <div>{offer.owner.account.username}</div>
         </div>
       )}
     </div>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,18 @@
+import { Link } from "react-router-dom";
+
 import vintedlogo from "../../assets/vinted_logo.png";
+
 import "./Header.scss";
 
 const Header = () => {
   return (
     <header>
-      <img src={vintedlogo} alt="vinted-logo" />
+      <Link to="/">
+        <img src={vintedlogo} alt="vinted-logo" />
+      </Link>
       <div>
-        <button>S'inscrire</button>
-        <button>Se connecter</button>
+        <button className="thin">S'inscrire</button>
+        <button className="thin">Se connecter</button>
         <button>Vends tes articles</button>
       </div>
     </header>
